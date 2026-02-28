9fbc0dd046223b4e701376d1c43f1ab74168db9e
Afeef <afeefaaban747@gmail.com>
2026-02-28 16:06:16 +0530

    added double jump and gun and bullet

---
+++ gun.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var rotational_offset: Marker2D = $Rotational_offset
+@onready var shoot_pos: Marker2D = $Rotational_offset/ColorRect/Shoot_pos
+
+var bullet = preload("res://bullet.tscn")
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var mouse_pos = get_global_mouse_position()
+	var mouse_vector = mouse_pos - rotational_offset.global_position
+	rotation = mouse_vector.angle()
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+		
+		
+func shoot():
+	var new_bullet = bullet.instantiate()
+	get_tree().root.add_child(new_bullet)
+	new_bullet.global_position = shoot_pos.global_position
+	new_bullet.global_rotation = shoot_pos.global_rotation
--- player.gd
+++ player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -600.0
 var jumps_left = 2
-
+var bullet = preload("res://bullet.tscn")
 func _physics_process(delta: float) -> void:
+	print(jumps_left)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -16,15 +17,17 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 	if jumps_left>2:
 		jumps_left = 2
-		#hehe
-		
 	if is_on_floor():
-		jumps_left= 2
+		jumps_left = 1
+
+		#hehe
 	var direction := Input.get_axis("Left", "Right")
-	if Input.is_action_pressed("sprint"):
-		velocity *= 2
+
 	if direction:
-		velocity.x = direction * SPEED
+		if Input.is_action_pressed("sprint"):
+			velocity = 1.1 * velocity
+		else:
+			velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
